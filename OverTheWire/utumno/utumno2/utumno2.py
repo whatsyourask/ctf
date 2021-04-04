ccaeb25ef86f2f5d33819512756eb4b06fa3217f
whatsyourask <lexeymartynov@gmail.com>
2021-04-04 03:34:08 -0400

    There is no way to exploit it remote, I think, only maybe to send commands for writting a C code and then run it

--- OverTheWire/utumno/utumno2/utumno2.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-
-
-con = ssh('utumno2', 'utumno.labs.overthewire.org', password='ceewaceiph', port=2227)
-p = con.process(executable='/utumno/utumno2', argv=[], env={"": "", "": "", "": "", "": "", "": "", "": "", "":"", "": "", "": "", "":""})
-gdb.attach(p, "b *main")
