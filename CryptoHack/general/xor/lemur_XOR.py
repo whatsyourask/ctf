99d3c9436d6b31536da91ce5dea3cb1158315dc2
whatsyourask <lexeymartynov@gmail.com>
2021-09-05 21:19:42 +0300

    Finally solved it with opencv

--- CryptoHack/general/xor/lemur_XOR.py
+++ CryptoHack/general/xor/lemur_XOR.py
@@ -1,12 +1,10 @@
 import sys
-from pwn import xor
-from binascii import unhexlify
+import cv2
 
-def get_file_content(filename):
-    return open(filename, 'rb').read() 
-
-
-lemur_img_filename = sys.argv[1]
-flag_img_filename = sys.argv[2]
-lemur_img = get_file_content(lemur_img_filename)
-flag_img = get_file_content(flag_img_filename)
+# https://www.geeksforgeeks.org/arithmetic-operations-on-images-using-opencv-set-2-bitwise-operations-on-binary-images/
+lemur = sys.argv[1]
+flag = sys.argv[2]
+lemur_img = cv2.imread(lemur)
+flag_img = cv2.imread(flag)
+xored_flag = cv2.bitwise_xor(lemur_img, flag_img)
+cv2.imwrite('flag.png', xored_flag)
